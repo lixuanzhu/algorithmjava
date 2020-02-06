eab326f8f79c6db981f0baaa9d5eaedf917801de
Sean Zhu <seanzhu@microsoft.com>
2020-02-05 23:19:25 -0800

    getLongestSubstring

--- LongestSubstring.java
+++ LongestSubstring.java
@@ -1,12 +1,34 @@
 import java.util.*;
 
 class LongestSubstring{
-    public static void main(String[] args){
-
-    }
-
-    public static int getLengthOfLongestSubstring(String s){
+    public static String getLongestSubstring(String str){
         Map<Character, Integer> map = new HashMap<>();
-        return 0;
+        int max = 0;
+        int maxstart = 0;
+        int start = 0;
+        for (int i  = 0; i< str.length(); i++) {
+            Character c = str.charAt(i);
+            if (map.containsKey(c)){
+                int index = map.get(c);
+                if (index >= start){
+                    if (max < i - start){
+                        max = i -start;
+                        maxstart = start;
+                    }
+                    start = index + 1;
+                }
+            }
+            map.put(c, i);
+        }
+        if( max < str.length() -start){
+            max = str.length() -start;
+            maxstart = start;
+        }
+        return str.substring(maxstart, maxstart + max);
+    }
+    public static void main(String[] args){
+        String str = "abcbdfdaccadef";
+        System.out.println(str);
+        System.out.println(getLongestSubstring(str));
     }
 }
