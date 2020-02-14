c4f1c85ae33f94f23678e4232d2ac90a7a2c8079
Sean Zhu <seanzhu@microsoft.com>
2020-02-13 23:47:11 -0800

    tree node

---
+++ TraverseTree.java
@@ -0,0 +1,99 @@
+import java.util.*;
+
+public class TraverseTree{
+    public static void preorderRecursive(TreeNode root, List<Integer> res){
+        if(root != null){
+            res.add(root.val);
+            preorderRecursive(root.left, res);
+            preorderRecursive(root.right, res);
+        }
+    }
+
+    public static void inorderRecursive(TreeNode root, List<Integer> res){
+        if(root != null){
+            inorderRecursive(root.left, res);
+            res.add(root.val);
+            inorderRecursive(root.right, res);
+        }
+    }
+
+    public static void postorderRecursive(TreeNode root, List<Integer> res){
+        if(root != null){
+            postorderRecursive(root.left, res);
+            postorderRecursive(root.right, res);
+            res.add(root.val);
+        }
+    }
+
+    public static void preorderIterative(TreeNode root, List<Integer> res){
+        Stack<TreeNode> s = new Stack<>();
+        while (s.size() > 0 || root != null){
+            while(root != null){
+                s.push(root);
+                res.add(root.val);
+                root = root.left;
+            }
+            root = s.pop();
+            root = root.right;
+        }
+    }
+
+    public static void inorderIterative(TreeNode root, List<Integer> res){
+        Stack<TreeNode> s = new Stack<>();
+        while (s.size() > 0 || root != null){
+            while(root != null){
+                s.push(root);
+                root = root.left;
+            }
+            root = s.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+    }
+
+    public static void postorderIterative(TreeNode root, List<Integer> res){
+        Stack<TreeNode> s = new Stack<>();
+        while (s.size() > 0 || root != null){
+            while(root != null){
+                s.push(root);
+                s.push(root);
+                root = root.left;
+            }
+            root = s.pop();
+            if(s.size() == 0) return; 
+            if(root == s.peek()) {
+                root = root.right;
+            } else {
+                res.add(root.val);
+                root = null;
+            }
+        }
+    }
+
+    public static void postorderIterative2(TreeNode root, List<Integer> res){
+        Stack<TreeNode> s = new Stack<>();
+        while (s.size() > 0 || root != null){
+            while(root != null){
+                s.push(root);
+                res.add(0, root.val);
+                root = root.right;
+            }
+            root = s.pop();
+            root = root.left;
+        }
+    }
+
+    public static void main(String[] args){
+        List<Integer> res = new LinkedList<>();
+        String str = "0 1 2 3 4 5 6";
+        TreeNode root = TreeNode.fromString(str);
+        postorderIterative(root, res);
+        print(res);
+    }
+
+    private static void print(List<Integer> res){
+        for(Integer i : res){
+            System.out.printf(i + " ");
+        }
+    }
+}
---
+++ TreeNode.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int x){
+        val = x;
+    }
+
+    public static String toString(TreeNode root){
+        String res = "";
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(q.size() != 0){
+            TreeNode cur = q.poll();
+            if(cur == null) {
+                res = res + " n";
+                continue;
+            }
+            res = res + " " + cur.val;
+            q.add(cur.left);
+            q.add(cur.right);
+        }
+        return res;
+    }
+
+    public static TreeNode fromString(String str){
+        String[] arr = str.split(" ");
+        TreeNode res = new TreeNode(Integer.parseInt(arr[0]));
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(res);
+
+        for(int i = 1; i < arr.length; i++){
+            TreeNode cur = q.poll();
+            if(!arr[i].equals("n")){
+                cur.left = new TreeNode(Integer.parseInt(arr[i]));
+                q.add(cur.left);
+            }
+            if(i < arr.length - 1) i++;
+            if(!arr[i].equals("n")){
+                cur.right = new TreeNode(Integer.parseInt(arr[i]));
+                q.add(cur.right);
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args){
+        String str = "1 2 3 n 5 6";
+        TreeNode node = fromString(str);
+        String str2 = toString(node);
+        System.out.println(str2);
+    }
+}
