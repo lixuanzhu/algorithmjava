29609aa7f1570721f453b57af84dc389f675dad7
Lixuan Zhu <lixuanzhu@gmail.com>
2020-05-31 17:03:29 -0700

    traverse tree with pair data structure

--- help/Util.java
+++ help/Util.java
@@ -1,5 +1,7 @@
 package help; 
 
+import java.util.*;
+
 public class Util{
     public static void swap(int[] arr, int x, int y){
         int temp = arr[x];
@@ -7,8 +9,15 @@ public class Util{
         arr[y] = temp;
     }
 
-    public static void print(int[] arr){
-        for (int n : arr) {
+    public static void print(List<Integer> list){
+        for (int n : list) {
+            System.out.print(n + "\t");
+        }
+        System.out.println();
+    }
+
+    public static void print(int[] list){
+        for (int n : list) {
             System.out.print(n + "\t");
         }
         System.out.println();
