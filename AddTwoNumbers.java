a4310b3175e291981d2b8f428ea3e32ab5b4375c
Lixuan Zhu <lixuanzhu@gmail.com>
2020-06-22 17:38:25 -0700

    simplidy

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -13,29 +13,23 @@ class AddTwoNumbers
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2){
         ListNode dummy = new ListNode(0);
-        ListNode p = l1;
-        ListNode q = l2;
         ListNode cur = dummy;
         int carry  = 0;
-        while( p!= null || q != null){
-            int x = p != null ? p.val : 0;
-            int y = q != null ? q.val : 0;
-            int sum = x+ y + carry;
+        while (l1!= null || l2 != null || carry !=0){
+            int sum = carry;
+            if (l1 != null){
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null){
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            
             carry = sum / 10;
             cur.next = new ListNode(sum % 10);
             cur = cur.next;
-
-            if(p != null){
-                p = p.next;
-            }
-
-            if(q != null){
-                q = q.next;
-            }
-        }
-
-        if(carry > 0){
-            cur.next = new ListNode(carry);
         }
 
         return dummy.next;
